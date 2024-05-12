3afb0837bd43f382e127ca63760c4967ca6a0ca0
Dániel Barabás <73437878+DanielBarabas@users.noreply.github.com>
2024-05-12 23:09:57 +0200

    first commit

---
+++ app_checkpoint.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+st.set_page_config(
+    page_title= "CodeFreePredictor"
+)
+st.title("Data Upload page")
+st.sidebar.success("Select a page above.")
+
+user_file = st.sidebar.file_uploader(label="You can upload the data here.",
+                                     type = ['csv','xlsx'])
+if user_file is not None:
+    try:
+        df = pd.read_csv(user_file)
+    except Exception as e:
+        print(e)
+        df = pd.read_excel(user_file)
+
+st.write(df.head(10))
