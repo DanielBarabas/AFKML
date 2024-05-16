8a3b5369afa2e23dbfd2816d9f31af7f6614e6eb
Dániel Barabás <73437878+DanielBarabas@users.noreply.github.com>
2024-05-16 13:10:34 +0200

    minimal profiling

--- pages/2_Pandas_profiling.py
+++ pages/2_Pandas_profiling.py
@@ -1,3 +1,12 @@
+import pandas as pd
+import ydata_profiling
 import streamlit as st
+from streamlit_pandas_profiling import st_profile_report
 
 st.set_page_config(page_title="Homepage", page_icon="🐼")
+
+df = pd.read_csv("C:\Projects\Rajk\prog_2\project\prog_machine_project\data\smoking_driking_dataset_Ver01.csv")
+
+pr = df.profile_report(minimal = True)
+
+st_profile_report(pr)
