039bb7fbac4bab5fe8205cccb7239fb3dfb9f3d8
AronNemeth <aron.nemeth8@gmail.com>
2024-05-16 10:46:39 +0200

    rename

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -1,13 +1,14 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title="AFKMLOMG")
+st.set_page_config(page_title="Homepage", page_icon="🏠")
 st.title("Data Upload page")
 st.sidebar.success("Select a page above.")
 
 user_file = st.sidebar.file_uploader(
     label="You can upload the data here.", type=["csv", "xlsx"]
 )
+
 if user_file is not None:
     try:
         df = pd.read_csv(user_file)
@@ -15,4 +16,4 @@ if user_file is not None:
         print(e)
         df = pd.read_excel(user_file)
 
-st.write(df.head(10))
+# st.write(df.head(10))
---
+++ pages/2_Pandas_profiling.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.set_page_config(page_title="Homepage", page_icon="🐼")
