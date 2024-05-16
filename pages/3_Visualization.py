ba93a139e8ac26a2edc8b161875320996f216d23
Dániel Barabás <73437878+DanielBarabas@users.noreply.github.com>
2024-05-16 16:17:40 +0200

    vizu functions

--- pages/3_Visualization.py
+++ pages/3_Visualization.py
@@ -1,26 +1,33 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
+from vizualization_functions import boxplot , stacked_bar, scatter
 
-st.set_page_config(page_title="Visualization", page_icon="📈")
+st.set_page_config(page_title="Visualization", layout="wide")
 alt.data_transformers.disable_max_rows()
+cat_list = ["object","category"]
+exp = st.expander(label="Association Figure")
 
-exp =  st.expander(label="Association Figure")
-df = pd.read_csv("C:\Projects\Rajk\prog_2\project\prog_machine_project\data\smoking_driking_dataset_Ver01.csv")
-small_df = df.sample(n = 10_000)
+
+small_df =  st.session_state["df"].sample(n=10_000)
 with exp:
-    st.write("asdfg")
-    options = st.multiselect(label = "Choose the desired colums",
-                             options=df.columns, 
-                             default=df.columns[[1,2]].to_list(),
-                             max_selections=2)
+    options = st.multiselect(
+        label="Choose the desired colums",
+        options=small_df.columns,
+        default=small_df.columns[[1, 2]].to_list(),
+        max_selections=2,
+    )
     if len(options) == 2:
         st.write("Ok")
+        #Both category:
+        if small_df[options[0]].dtype in cat_list and small_df[options[1]].dtype in cat_list:
+            fig = stacked_bar(small_df,options)
+        #Both continous:
+        elif small_df[options[0]].dtype not in cat_list and small_df[options[1]].dtype not in cat_list:
+            fig = scatter(small_df, options)
+        # One is continous, other is not:
+        else:
 
-        cc_fig = alt.Chart(small_df).mark_bar().encode(
-            x=alt.X('count()').stack("normalize"),
-            y=options[0],
-            color=options[1]
-        )
-        st.altair_chart(cc_fig)
-    #st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
+            fig = boxplot(small_df,options)
+        st.altair_chart(fig)
+
---
+++ vizualization_functions.py
@@ -0,0 +1,36 @@
+import altair as alt
+import pandas as pd
+
+
+def stacked_bar(df, chosen_features):
+    fig = (
+        alt.Chart(df)
+        .mark_bar()
+        .encode(
+            x=alt.X("count()").stack("normalize"),
+            y=chosen_features[0],
+            color=chosen_features[1],
+        )
+    )
+    return fig
+
+
+def boxplot(df, chosen_features):
+    fig = (
+        alt.Chart(df)
+        .mark_boxplot(extent="min-max")
+        .encode(
+            alt.X(f"{chosen_features[0]}:Q").scale(zero=False),
+            alt.Y(f"{chosen_features[1]}:N"),
+        )
+    )
+    return fig
+
+
+def scatter(df, chosen_features):
+    fig = (
+        alt.Chart(df)
+        .mark_circle(size=60)
+        .encode(x=chosen_features[0], y=chosen_features[1])
+    )
+    return fig
