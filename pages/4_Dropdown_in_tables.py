dd758817ada7ee5ff4fc606187c92a80ad7960a0
Dániel Barabás <73437878+DanielBarabas@users.noreply.github.com>
2024-05-23 22:46:27 +0200

    encoding ready but dirty

--- pages/4_Dropdown_in_tables.py
+++ pages/4_Dropdown_in_tables.py
@@ -2,23 +2,29 @@ import streamlit as st
 import pandas as pd
 from st_aggrid import GridOptionsBuilder, AgGrid
 
+
 @st.cache_data
 def load_data(path):
     return pd.read_csv(path)
-df = load_data(
-    "C:\Projects\Rajk\prog_2\project\prog_machine_project\data\drinking.csv"
-)
+
+
+df = load_data("C:\Projects\Rajk\prog_2\project\prog_machine_project\data\drinking.csv")
 
 typelist = ("Numeric", "Categorical")
 
 dtype_map = {"Numeric": float, "Categorical": "category"}
-dtype_map_inverse = {'float64': "Numeric", "category": "Categorical","object":"Categorical",'int64': "Numeric"}
+dtype_map_inverse = {
+    "float64": "Numeric",
+    "category": "Categorical",
+    "object": "Categorical",
+    "int64": "Numeric",
+}
 
 # This can be cached:
 original_types = pd.DataFrame(
     {
-    "Variable": df.columns.to_list(), 
-    "Type": [dtype_map_inverse[a] for a in [str(typ) for typ in df.dtypes]]
+        "Variable": df.columns.to_list(),
+        "Type": [dtype_map_inverse[a] for a in [str(typ) for typ in df.dtypes]],
     }
 )
 orig_dict = {}
@@ -40,19 +46,15 @@ response = AgGrid(
     gridOptions=vgo,
 )
 
-res_dict = {}   
+res_dict = {}
 for i in range(len(response.data)):
     res_dict[response.data.iloc[i]["Variable"]] = response.data.iloc[i]["Type"]
 st.write(res_dict == orig_dict)
 for key in res_dict.keys():
     if res_dict[key] != orig_dict[key]:
         # Cannot handle exceptions yet
-        df[key] = df[key].astype(dtype_map[res_dict[key]])    
-orig_dict = res_dict       
+        df[key] = df[key].astype(dtype_map[res_dict[key]])
+orig_dict = res_dict
 st.write(df.dtypes)
 
-#Encoding part
-
-
-
-
+# Encoding part
--- pages/5_Encoding.py
+++ pages/5_Encoding.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import pandas as pd
 from st_aggrid import GridOptionsBuilder, AgGrid
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder, OrdinalEncoder, TargetEncoder
+from sklearn.preprocessing import (
+    LabelEncoder,
+    OneHotEncoder,
+    OrdinalEncoder,
+    TargetEncoder,
+)
 import numpy as np
 
 
@@ -11,6 +16,9 @@ def load_data(path):
 
 
 df = load_data("C:\Projects\Rajk\prog_2\project\prog_machine_project\data\drinking.csv")
+# Ezt azért csináltam, hogy meg tudjuk nézni multiclass esetben működik-e
+df["weight"] = df["weight"].astype("category")
+
 
 target_var = st.multiselect(
     "Choose the target variable:", df.columns.to_list(), max_selections=1
@@ -19,29 +27,7 @@ if len(target_var) > 0:
     target_var = target_var[0]
 else:
     target_var = df.columns.to_list()[0]
-st.write(target_var)
 
-
-def create_encoded_column(encoder, original_colname, df):
-    """
-    Use encoder to fit_transform the `original_colname` column and return just the encoded column.
-    """
-    # you fit on training data
-    encoded_column = encoder.fit_transform(df[original_colname].values.reshape(-1, 1))
-    return encoded_column
-
-
-def encode_target(target_var: str, df: pd.DataFrame, encoding: str = "One-hot"):
-    if encoding == "One-hot":
-        encoder = OneHotEncoder(drop="first", sparse_output=False, dtype=np.int64)
-    elif encoding == "Target":
-        pass
-    else:
-        pass
-    return create_encoded_column(encoder, target_var, df)
-
-
-target_encoded = encode_target(target_var, df)
 
 encodings = ("One-Hot", "Target", "Ordinal")
 dtype_map = {"Numeric": float, "Categorical": "category"}
@@ -60,7 +46,67 @@ valid_cols = [
     )
 ]
 
-original_encodings = pd.DataFrame({"Variable": valid_cols, "Encoding": "Ordinal"})
+y_type = dtype_map_inverse[str(df[target_var].dtype)]
+
+st.write(target_var)
+st.write(y_type)
+
+
+def one_hot_encoding(df, x_column):
+    encoder = OneHotEncoder(drop="first", sparse_output=False, dtype=np.int8)
+    encoded_column = encoder.fit_transform(df[x_column].values.reshape(-1, 1))
+    encoded_column_name = [
+        x_column + "_one_hot_" + str(cat) for cat in encoder.categories_[0][1:]
+    ]
+    return encoded_column, encoded_column_name
+
+
+# TODO Check if multicollinearity can be a problem here
+def tartet_encoding(df, x_column, target_column, coltype):
+    x = df[x_column].to_numpy().reshape(-1, 1)
+    if coltype == "Numeric":
+        encoder = TargetEncoder(smooth="auto", target_type="continuous")
+        encoded_column = encoder.fit_transform(X=x, y=df[target_column])
+        encoded_column_name = x_column + "_target"
+    else:
+        encoder = TargetEncoder(smooth="auto", target_type="auto")
+        encoded_column = encoder.fit_transform(X=x, y=df[target_column])
+        if encoder.target_type_ == "binary":
+            encoded_column_name = x_column + "_target"
+        else:
+            encoded_column_name = [
+                x_column + "_target_" + str(cat) for cat in encoder.classes_
+            ]
+
+    return encoded_column, encoded_column_name
+
+
+def ordinal_encoding(df, x_column):
+    oe = OrdinalEncoder(handle_unknown="use_encoded_value", unknown_value=np.nan)
+    encoded_column = oe.fit_transform(df[x_column].values.reshape(-1, 1))
+    encoded_column_name = x_column + "_ordinal"
+    return encoded_column, encoded_column_name
+
+
+def create_encoded_column(
+    encoding, x_column, df, target_column="", y_type="Categorical"
+):
+    """
+    Use encoder to fit_transform the `original_colname` column and return just the encoded column.
+    """
+    if encoding == "One-Hot":
+        encoded_column, encoded_column_name = one_hot_encoding(df=df, x_column=x_column)
+    elif encoding == "Target":
+        encoded_column, encoded_column_name = tartet_encoding(
+            df, x_column, target_column, coltype=y_type
+        )
+    elif encoding == "Ordinal":
+        encoded_column, encoded_column_name = ordinal_encoding(df, x_column)
+
+    return pd.DataFrame(encoded_column), encoded_column_name
+
+
+original_encodings = pd.DataFrame({"Variable": valid_cols, "Encoding": "One-Hot"})
 
 gb = GridOptionsBuilder.from_dataframe(original_encodings)
 gb.configure_column(
@@ -75,3 +121,24 @@ response = AgGrid(
     original_encodings,
     gridOptions=vgo,
 )
+encoding_dict = {}
+for _, row in response.data.iterrows():
+    encoding_dict[row[0]] = row[1]
+
+st.write(encoding_dict)
+
+data_list = []
+col_list = []
+
+
+for _, row in response.data.iterrows():
+    encoded_col, encoded_colname = create_encoded_column(
+        encoding=row[1], x_column=row[0], df=df, target_column=target_var, y_type=y_type
+    )
+    encoded_col.columns = encoded_colname
+    data_list.append(encoded_col)
+    col_list.append(encoded_colname)
+
+st.write(col_list)
+for data in data_list:
+    st.write(data)
