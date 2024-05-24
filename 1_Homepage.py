37575b7cd0e4dd85af1a56566bf37e4d2442b3bc
Dániel Barabás <73437878+DanielBarabas@users.noreply.github.com>
2024-05-24 12:39:27 +0200

    homepage updated

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-import data_wrangling as dw
-
+import pages.modules.data_wrangling as dw
+from st_aggrid import GridOptionsBuilder, AgGrid
 
 st.set_page_config(page_title="Home page", layout="wide")
 st.title("Data Upload page")
@@ -10,6 +10,7 @@ st.title("Data Upload page")
 user_file = st.file_uploader(
     label="You can upload the data here.", type=["csv", "xlsx"]
 )
+typelist = ("Numeric", "Categorical")
 
 # TODO add compressed upload option
 # TODO what if user wants to upload another data? - session state if statement doesn't allow that (create refresh page button)
@@ -36,8 +37,25 @@ if "df" in st.session_state:
         )
     )
 
-    ### Recast datatypes
-    vars_recast = st.multiselect(
-        "Choose variables to set data types", st.session_state["df"].dtypes.index
+    original_types = dw.create_type_df(df=st.session_state["df"])
+    orig_dict = dw.create_type_dict(original_types)
+
+    gb = GridOptionsBuilder.from_dataframe(original_types)
+    gb.configure_column(
+        "Type",
+        editable=True,
+        cellEditor="agSelectCellEditor",
+        cellEditorParams={"values": typelist},
     )
-    df = dw.cast_dtypes(st.session_state["df"], vars_recast)
+
+    vgo = gb.build()
+    response = AgGrid(
+        original_types,
+        gridOptions=vgo,
+    )
+
+    res_dict = dw.create_type_dict(response.data)
+
+    df, orig_dict = dw.cast_dtype(st.session_state["df"], orig_dict, res_dict)
+
+    st.write(st.session_state["df"].dtypes)
