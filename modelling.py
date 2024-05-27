f948923166db999f89e1dab5732eab602b73e8c5
AronNemeth <aron.nemeth8@gmail.com>
2024-05-27 16:19:41 +0200

    modelling ready for ensemble reg and class

--- modelling.py
+++ modelling.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import mean_squared_error
+import streamlit as st
 
 
+# TODO return np arrays instead -> faster? (csak ebben az esetben van értelme ennek az fv-nek)
 def train_test_X_y_split(df: pd.DataFrame, y_colname: str, test_ratio: float = 0.2):
     """Creates test/train and X/y split of a df
 
@@ -32,34 +32,84 @@ def train_test_X_y_split(df: pd.DataFrame, y_colname: str, test_ratio: float = 0
     ]
 
 
-# TODO modify input param dtypes based on Barab's encoding
-# TODO Also return source as Mor's code?
-def rand_for(
-    params: dict,
-    loss_func: str,
-    feat_used: list,
-    X_train: pd.DataFrame,
-    X_test: pd.DataFrame,
-    y_train: pd.DataFrame,
-    y_test: pd.DataFrame,
-) -> list[pd.DataFrame]:
-    """Fits a random forest and evaluates it with chosen loss functions
+# TODO mások a paraméterek regressziónál
+def rf_param_input(n_features):
+    """Gives ui to specify the parameters of random forest
 
     Args:
-        params (dict): hyperparameters for the random forest
-        loss_func (str): loss function
-        feat_used (list): column names to be included in modelling
-        X_train (pd.DataFrame):
-        X_test (pd.DataFrame):
-        y_train (pd.DataFrame):
-        y_test (pd.DataFrame):
+        n_features (int): number of features
 
     Returns:
-        list[pd.DataFrame]: rmse, rf
+        tuple: specified parameter values
     """
-    rf = RandomForestClassifier(**params).fit(X_train.loc[:, feat_used], y_train)
-    test_preds = rf.predict(X_test.loc[:, feat_used])
-    if loss_func == "rmse":
-        rmse = mean_squared_error(y_true=y_test, y_pred=test_preds)
+    n_estimators = st.slider("n_estimators", min_value=10, max_value=1000, value=100)
+    max_depth = st.radio(
+        "max_depth", options=["Up to min_samples_split", "Input specific number"]
+    )
+    if max_depth == "Input specific number":
+        st.slider("max_depth", min_value=0, max_value=15, value=4)
+    else:
+        max_depth = None
 
-    return rmse, rf
+    max_features = st.radio(
+        "max_features", options=["All", "Square root", "Log", "Input specific number"]
+    )
+    if max_features == "Input specific number":
+        st.slider("max_features", min_value=1, max_value=n_features, value=1)
+    elif max_features == "All":
+        max_features = None
+
+    min_samples_split = st.number_input(
+        "min_samples_split", min_value=1, value=2, step=1
+    )
+    min_samples_leaf = st.number_input("min_samples_leaf", min_value=1, value=1, step=1)
+    bootstrap = st.checkbox("Boostrap", value=True)
+    paralel = st.checkbox(
+        "Do you want to build trees in paralel with multiple CPUs", value=True
+    )
+    if paralel:
+        n_jobs = -1
+
+    return (
+        n_estimators,
+        max_depth,
+        max_features,
+        min_samples_split,
+        min_samples_leaf,
+        bootstrap,
+        n_jobs,
+    )
+
+
+def xbg_param_input():
+    n_estimators = st.slider("n_estimators", min_value=10, max_value=1000, value=100)
+    max_depth = st.slider("max_depth", min_value=0, max_value=15, value=6)
+    learning_rate = st.slider(
+        "learning_rate", min_value=0.0, max_value=1.0, value=0.3, step=0.01
+    )
+    gamma = st.number_input("gamma", min_value=0.0, value="min", step=0.05)
+    subsample = st.slider(
+        "subsample", min_value=0.1, max_value=1.0, value=1.0, step=0.01
+    )
+    colsample_bytree = st.slider(
+        "colsample_bytree", min_value=0.1, max_value=1.0, value=1.0, step=0.01
+    )
+    reg_lambda = st.number_input("lamdba", min_value=0.0, value=1.0, step=0.1)
+    reg_alpha = st.number_input("aplha", min_value=0.0, value=1.0, step=0.1)
+    # TODO add step?
+    min_child_weight = st.number_input("min_child_weight", min_value=0, value=1)
+    # TODO add step?
+    scale_pos_weight = st.number_input("scale_pos_weight", min_value=0, value=1)
+
+    return (
+        n_estimators,
+        max_depth,
+        learning_rate,
+        gamma,
+        subsample,
+        colsample_bytree,
+        reg_lambda,
+        reg_alpha,
+        min_child_weight,
+        scale_pos_weight,
+    )
--- pages/5_Modelling.py
+++ pages/5_Modelling.py
@@ -1,13 +1,19 @@
 import streamlit as st
 import pandas as pd
-from modelling import train_test_X_y_split
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from xgboost import XGBClassifier, XGBRegressor
+import numpy as np
+import modelling as m
 
 # TODO töröl modelling a pages mappából
+# TODO rewrite, so not everything is run automatically
 
 st.set_page_config(page_title="Modelling", layout="wide")
 st.title("Modelling")
 
 
+### replace with session state df
 @st.cache_data
 def load_data(path):
     df = pd.read_csv(path)
@@ -19,9 +25,32 @@ df = load_data(
     "C:/Users/aronn/OneDrive/Asztali gép/Rajk/Prog 2/prog_machine_project/data/smoking_drinking.csv"
 )
 
+oh_enc = OneHotEncoder(drop="first", sparse_output=False, dtype=np.int8)
+
+df_ = pd.DataFrame(oh_enc.fit_transform(df[["sex"]].values.reshape(-1, 1)))
+df_colnames_oh = [str(f) for f in oh_enc.categories_[0][1:]]
+df_.columns = df_colnames_oh
+df = df_.join(df)
+
+df_ = pd.DataFrame(oh_enc.fit_transform(df[["DRK_YN"]].values.reshape(-1, 1)))
+df_colnames_oh = [str(f) for f in oh_enc.categories_[0][1:]]
+df_.columns = df_colnames_oh
+df = df_.join(df)
+
+df = df.rename(columns={"Y": "is_drinking"})
+df = df.drop(columns=["sex", "DRK_YN"])
+
 st.write(df.head(10))
+#### replace up to that point
 
 with st.expander(label="Preparation", expanded=True):
+    problem_type = st.selectbox(
+        "Choose the type of your problem",
+        options=["Regression", "Multi-classification", "Binary-classification"],
+    )
+
+    model_type = st.selectbox("Select model", options=["Random forest", "XGBoost"])
+
     test_ratio = st.slider(
         "Select what ratio you want the test set to be",
         min_value=0.1,
@@ -30,58 +59,112 @@ with st.expander(label="Preparation", expanded=True):
         help="Normally test set ratio is between 0.1 and 0.3",
     )
 
-    X_train, X_test, y_train, y_test = train_test_X_y_split(
-        df=df, y_colname="DRK_YN", test_ratio=test_ratio
+    X_train, X_test, y_train, y_test = m.train_test_X_y_split(
+        df=df, y_colname="triglyceride", test_ratio=test_ratio
     )
 
     st.write(len(X_train), len(X_test), len(y_train), len(y_test))
 
-    model_type = st.selectbox("Select model", options=["Random forest"])
-
-    # not live ATM
-    # TODO dynmically update loss func options based on model
-    model_lossf_map = {}
-    loss_func = st.selectbox("Select loss function", options=["RMSE", "MSE", "MAE"])
-
     use_all = st.checkbox("Use all features?", value=True)
     # TODO here the encoded cat variables might cause an issue
-    if not use_all:
+    if use_all:
+        feat_used = X_train.columns
+    else:
         feat_used = st.multiselect(
             "Choose the features to be included in modeling", options=X_train.columns
         )
 
-model_params_map = {
-    "random_forest": [
-        "n_estimators",
-        "max_depth",
-        "max_features",
-        "min_samples_split",
-        "min_samples_leaf",
-        "bootstrap",
-        "n_jobs",
-    ],
-    "xgboost": [],
-    "decision_tree": [],
-}
+    n_features = X_train.shape[1]
 
-with st.expander(label="Parameters"):
-    n_estimators = st.slider("n_estimators", min_value=10, max_value=1000, value=100)
-    # max_depth = -> problem: defaults to None
-    max_features = st.radio(
-        "max_features", options=["All", "Square root", "Log", "Specify in integers"]
-    )
 
-    if max_features == "Specify in integers":
-        st.slider("max_features", min_value=1, max_value=X_train.shape[1], value=1)
+with st.expander(label="Hyper-parameters"):
+    if model_type == "Random forest":
+        (
+            n_estimators,
+            max_depth,
+            max_features,
+            min_samples_split,
+            min_samples_leaf,
+            bootstrap,
+            n_jobs,
+        ) = m.rf_param_input(n_features)
 
-    min_samples_split = st.number_input(
-        "min_samples_split", min_value=1, value=2, step=1
-    )
-    min_samples_leaf = st.number_input("min_samples_leaf", min_value=1, value=1, step=1)
-    bootstrap = st.checkbox("Boostrap", value=True)
-    paralel = st.checkbox(
-        "Do you want to build trees in paralel with multiple CPUs", value=True
-    )
-    if paralel:
-        n_jobs = -1
+        if problem_type == "Regression":
+            if st.button("Run model", key="rfr"):
+                rf = RandomForestRegressor(
+                    n_estimators=n_estimators,
+                    max_depth=max_depth,
+                    max_features=max_features,
+                    min_samples_split=min_samples_split,
+                    min_samples_leaf=min_samples_leaf,
+                    bootstrap=bootstrap,
+                    n_jobs=n_jobs,
+                ).fit(X_train.loc[:, feat_used], y_train)
+
+                st.write(type(rf))
+        else:
+            if st.button("Run model", key="rfc"):
+                rf = RandomForestClassifier(
+                    n_estimators=n_estimators,
+                    max_depth=max_depth,
+                    max_features=max_features,
+                    min_samples_split=min_samples_split,
+                    min_samples_leaf=min_samples_leaf,
+                    bootstrap=bootstrap,
+                    n_jobs=n_jobs,
+                ).fit(X_train.loc[:, feat_used], y_train)
+
+                st.write(type(rf))
+
+    elif model_type == "XGBoost":
+        (
+            n_estimators,
+            max_depth,
+            learning_rate,
+            gamma,
+            subsample,
+            colsample_bytree,
+            reg_lambda,
+            reg_alpha,
+            min_child_weight,
+            scale_pos_weight,
+        ) = m.xbg_param_input()
+
+        if problem_type == "Regression":
+            if st.button("Run model", key="xgbr"):
+                xgb = XGBRegressor(
+                    n_estimators=n_estimators,
+                    max_depth=max_depth,
+                    learning_rate=learning_rate,
+                    gamma=gamma,
+                    subsample=subsample,
+                    colsample_bytree=colsample_bytree,
+                    reg_lambda=reg_lambda,
+                    reg_alpha=reg_alpha,
+                    min_child_weight=min_child_weight,
+                    scale_pos_weight=scale_pos_weight,
+                ).fit(X_train.loc[:, feat_used], y_train)
+
+                st.write(type(xgb))
+                st.write(xgb.evals_result)
+                st.write(xgb.score)
+        else:
+            if st.button("Run model", key="xgbc"):
+                xgb = XGBClassifier(
+                    n_estimators=n_estimators,
+                    max_depth=max_depth,
+                    learning_rate=learning_rate,
+                    gamma=gamma,
+                    subsample=subsample,
+                    colsample_bytree=colsample_bytree,
+                    reg_lambda=reg_lambda,
+                    reg_alpha=reg_alpha,
+                    min_child_weight=min_child_weight,
+                    scale_pos_weight=scale_pos_weight,
+                ).fit(X_train.loc[:, feat_used], y_train)
+
+                st.write(type(xgb))
+                st.write(xgb.evals_result)
+                st.write(xgb.score)
+
     # TODO write out: your model is in training + time passed
