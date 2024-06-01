bb0e8a3e4000f50e7697fc544a61e38f567415f9
AronNemeth <aron.nemeth8@gmail.com>
2024-06-01 12:14:31 +0200

    tests added

--- test_pages/test_modules.py
+++ test_pages/test_modules.py
@@ -1,15 +1,40 @@
-from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium import webdriver
-import time
 
-def change_page(driver,page_name):
-     """Go to given page
 
-     Args:
-         driver (_type_): driver we use
-         page_name (_type_): page to go to
-     """
-     driver.find_element(By.LINK_TEXT, page_name).click()
+def test_change_page(driver, page_name):
+    link_element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, page_name))
+    )
+    if not link_element:
+        raise AssertionError(f"Could not find page with name {page_name}")
+
+    link_element.click()
+
+
+def test_data_upload(driver, file_path: str):
+    file_input = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "input[type='file']"))
+    )
+    if not file_input:
+        raise AssertionError("Could not find file input")
+    file_input.send_keys(file_path)
+
+
+def test_wait_xpath(driver, x_path: str):
+    assert WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH, x_path))
+    ), f"Could not find element by {By.XPATH} with value {x_path}"
+
+
+def test_scroll_to_xpath(driver, x_path: str):
+    elem = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, x_path))
+    )
+    if not elem:
+        raise AssertionError(
+            f"Could not find element by {By.XPATH} with value {x_path}"
+        )
+
+    driver.execute_script("arguments[0].scrollIntoView();", elem)
