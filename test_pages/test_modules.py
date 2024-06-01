c6def1f2bfd7eff6c083cc21e6fd875875d9a3f3
Dániel Barabás <73437878+DanielBarabas@users.noreply.github.com>
2024-06-01 15:23:51 +0200

    adf

--- test_pages/test_modules.py
+++ test_pages/test_modules.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 
 ###### General ######
@@ -40,6 +41,17 @@ def test_scroll_to_xpath(driver, x_path: str):
 
     driver.execute_script("arguments[0].scrollIntoView();", elem)
 
+def test_scroll_to_tagname(driver, tag_name: str):
+    elem = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.TAG_NAME, tag_name))
+    )
+    if not elem:
+        raise AssertionError(
+            f"Could not find element by {By.XPATH} with value {tag_name}"
+        )
+
+    driver.execute_script("arguments[0].scrollIntoView();", elem)
+
 
 ###### Homepage ######
 
@@ -51,3 +63,40 @@ def test_data_upload(driver, file_path: str):
     if not file_input:
         raise AssertionError("Could not find file input")
     file_input.send_keys(file_path)
+
+
+###### EDA ######
+def click_toggle(toggles,toggle_num):
+     try:
+          toggle = toggles[toggle_num]
+          toggle.click()
+     except:
+        raise AssertionError(
+            f"Could not find the {toggle_num}th toggle on the page"
+        )
+
+def scroll_to_h2(driver,h2s,h2_num):
+     try:
+          h2 = h2s[h2_num]
+          driver.execute_script("arguments[0].scrollIntoView();", h2)
+     except:
+         raise AssertionError(
+            f"Could not find the {h2_num}th header on the page"
+        )
+        
+
+def find_h2s(driver):
+     try:
+          return driver.find_elements(By.TAG_NAME, "h2")
+     except:
+         raise AssertionError(
+            "No h2 headers on the page"
+        )
+         
+def find_toggles(driver):
+     try:
+          return driver.find_elements(By.CLASS_NAME, "st-b4.st-b6")
+     except:
+         raise AssertionError(
+            "No toggles headers on the page"
+        )
