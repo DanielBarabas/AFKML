e65f7f8491b3baa12c09625b2fe9ffad58858dbc
AronNemeth <aron.nemeth8@gmail.com>
2024-06-11 11:26:46 +0200

    eval page commit (only to make checkout work again)

--- pages/6_Evaluation.py
+++ pages/6_Evaluation.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_extras.stateful_button import button
 from modules.eval import (
     binary_roc,
     binary_prec_recall,
@@ -7,7 +8,8 @@ from modules.eval import (
     feature_importance,
     multiclass_cm,
     multiclass_roc,
-    reg_table
+    reg_table,
+    predict1,
 )
 
 
@@ -24,57 +26,6 @@ elif "model" not in st.session_state:
 
 st.title("Model evaluation")
 
-######### Cached functions ###########
-@st.cache_data
-def predict1(_model, X_test, problem_type):
-    if problem_type == "Regression":
-        y_pred = _model.predict(X_test)
-        y_pred_binary = "anyad"
-    else:
-        y_pred = _model.predict_proba(X_test)
-        y_pred_binary = _model.predict(X_test)
-
-    return y_pred, y_pred_binary
-@st.cache_resource(experimental_allow_widgets=True)
-def show_bin_roc_chart(y_pred,y_test):
-    st.altair_chart(binary_roc(y_pred, y_test))
-
-@st.cache_resource(experimental_allow_widgets=True)
-def show_prec_recall_chart(y_pred,y_test):
-    st.altair_chart(binary_prec_recall(y_pred, y_test))
-
-@st.cache_resource(experimental_allow_widgets=True)
-def show_bin_cm_chart(y_pred_binary,y_test):
-    st.altair_chart(binary_cm(y_pred_binary, y_test))
-@st.cache_resource(experimental_allow_widgets=True)
-def show_bin_metric_table(y_pred_binary,y_test):
-    st.write(binary_metric_table(y_pred_binary, y_test))
-
-@st.cache_resource(experimental_allow_widgets=True)
-def show_feature_importance(_model,X_test,y_test):
-    st.altair_chart(
-            feature_importance(
-                _model,
-                X_test,
-                y_test,
-            )
-        )
-
-@st.cache_resource(experimental_allow_widgets=True)
-def show_multi_roc(y_pred,y_test,_le):
-    st.altair_chart(
-                multiclass_roc(y_pred, y_test, _le)
-            )
-    
-@st.cache_resource(experimental_allow_widgets=True)
-def show_multi_cm(y_pred_binary,y_test,_le):
-    st.altair_chart(
-                multiclass_cm(y_pred_binary, y_test, _le)
-            )
-
-@st.cache_resource(experimental_allow_widgets=True)
-def show_reg_table(y_pred,y_test):       
-    st.write(reg_table(y_pred,y_test))
 
 y_pred, y_pred_binary = predict1(
     st.session_state["model"],
@@ -85,56 +36,67 @@ y_pred, y_pred_binary = predict1(
 
 if st.session_state["problem_type"] == "Binary classification":
     st.header("ROC curve")
-    roc_switch = st.toggle("Create ROC Curve")
-    if roc_switch:
-        show_bin_roc_chart(y_pred,st.session_state["y_test"]) 
+    if button(label="Create ROC Curve", key="bi_roc"):
+        st.altair_chart(binary_roc(y_pred, st.session_state["y_test"]))
 
     st.header("Precision-Recall Chart")
-    pr_switch = st.toggle(label="Create Precision-Recall Chart")
-    if pr_switch:
-        show_prec_recall_chart(y_pred,st.session_state["y_test"])
+    if button(label="Create Precision-Recall Chart", key="bi_prec_rec"):
+        st.altair_chart(binary_prec_recall(y_pred, st.session_state["y_test"]))
 
     st.header("Confusion matrix")
-    cm_switch = st.toggle(label="Create Confusion Matrix")
-    if cm_switch:
-        show_bin_cm_chart(y_pred_binary,st.session_state["y_test"]) 
+    if button(label="Create Confusion Matrix", key="conf_matrix"):
+        st.altair_chart(binary_cm(y_pred_binary, st.session_state["y_test"]))
 
     st.header("Metric table")
-    mt_switch = st.toggle(label="Create Metric Table")
-    if mt_switch:
-        show_bin_metric_table(y_pred_binary,st.session_state["y_test"])
-        
+    if button(label="Create Metric Table", key="bi_metric_tab"):
+        st.write(binary_metric_table(y_pred_binary, st.session_state["y_test"]))
 
     st.header("Feature importance")
-    fi_switch = st.toggle(label="Create Feature Importance Chart")
-    if fi_switch:
-        show_feature_importance(st.session_state["model"],st.session_state["X_test"],st.session_state["y_test"])
-        
+    if button(label="Create Feature Importance Chart", key="bi_imp"):
+        st.altair_chart(
+            feature_importance(
+                st.session_state["model"],
+                st.session_state["X_test"],
+                st.session_state["y_test"],
+            )
+        )
 
 elif st.session_state["problem_type"] == "Multiclass classification":
     st.header("Feature importance")
-    fi_switch = st.toggle(label="Create Feature Importance Chart")
-    if fi_switch:
-        show_feature_importance(st.session_state["model"],st.session_state["X_test"],st.session_state["y_test"])
+    if button(label="Create Feature Importance Chart", key="m_imp"):
+        st.altair_chart(
+            feature_importance(
+                st.session_state["model"],
+                st.session_state["X_test"],
+                st.session_state["y_test"],
+            )
+        )
 
     st.header("Roc curves")
-    roc_switch = st.toggle(label="Create ROC Curves")
-    if roc_switch:
-        show_multi_roc(y_pred,st.session_state["y_test"],st.session_state["le"])
-        
+    if button(label="Create ROC Curves", key="m_roc"):
+        st.altair_chart(
+            multiclass_roc(y_pred, st.session_state["y_test"], st.session_state["le"])
+        )
 
     st.header("Multiclass confusion matrices")
-    cm_switch = st.toggle(label="Create Multiclass Confusion Matrices")
-    if cm_switch:
-        show_multi_cm(y_pred_binary,st.session_state["y_test"],st.session_state["le"])
+    if button(label="Create Multiclass Confusion Matrices", key="m_conf_matrix"):
+        st.altair_chart(
+            multiclass_cm(
+                y_pred_binary, st.session_state["y_test"], st.session_state["le"]
+            )
+        )
 
 else:
     st.header("Metric table")
-    mt_switch = st.toggle(label="Create Metric Table")
-    if mt_switch:
-        show_reg_table(y_pred,st.session_state["y_test"])
+    if button(label="Create Metric Table", key="reg_met_tab"):
+        st.write(reg_table(y_pred, st.session_state["y_test"]))
 
     st.header("Feature importance")
-    fi_switch = st.toggle(label="Create Feature Importance Chart")
-    if fi_switch:
-        show_feature_importance(st.session_state["model"],st.session_state["X_test"],st.session_state["y_test"])
+    if button(label="Create Feature Importance Chart", key="reg_imp"):
+        st.altair_chart(
+            feature_importance(
+                st.session_state["model"],
+                st.session_state["X_test"],
+                st.session_state["y_test"],
+            )
+        )
